5948a66371132be4373a7d8cc4f5ce65a74d3094
nicoodebug <nicoclaurui@gmail.com>
2024-11-28 02:48:43 -0300

    agregue una nueva url, que llame logout, con esto reemplace la url exit y por ultimo cree un def nuevo, el cual nos lleva al inicio cuando cerramos sesion

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.logout_view, name='logout'),
 ]
